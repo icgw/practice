4fdc94ea69349324b1beac79bda503d72e77e2b8
icgw <icgw@outlook.com>
2018-11-22 17:22:21 +0800

    Add Two Numbers

---
+++ Python3/0002._Add_Two_Numbers.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from dataStructures import ListNode
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
+        current, carry = dummy, 0
+        while l1 or l2 or carry:
+            val = carry;
+            if l1:
+                val += l1.val
+                l1 = l1.next
+            if l2:
+                val += l2.val
+                l2 = l2.next
+            carry = val // 10
+            current.next = ListNode(val % 10)
+            current = current.next
+        return dummy.next
+
+if __name__ == "__main__":
+    a, a.next, a.next.next = ListNode(2), ListNode(4), ListNode(3)
+    b, b.next, b.next.next = ListNode(5), ListNode(6), ListNode(4)
+    output = Solution().addTwoNumbers(a, b)
+    print("%d -> %d -> %d" 
+            % (output.val, output.next.val, output.next.next.val))
+
