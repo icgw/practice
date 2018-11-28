0bb00028de37333784d88c310bab1b14c823c0c2
icgw <icgw@outlook.com>
2018-11-28 21:16:44 +0800

    Search in Rotated Sorted Array

---
+++ C/0033._Search_in_Rotated_Sorted_Array.c
@@ -0,0 +1,18 @@
+int search(int* nums, int numsSize, int target){
+	int lo = 0, hi = numsSize - 1, mid;
+	while (lo < hi){
+		mid = (lo + hi) / 2;
+		if (nums[mid] < nums[hi]) hi = mid;
+		else lo = mid + 1;
+	}
+	int start = lo, realMid;
+	lo = 0, hi = numsSize - 1;
+	while (lo <= hi){
+		mid = (lo + hi) / 2;
+		realMid = (mid + start) % numsSize;
+		if (nums[realMid] == target) return realMid;
+		if (nums[realMid] < target) lo = mid + 1;
+		else hi = mid - 1;
+	}
+	return -1;
+}
--- C/Main.c
+++ C/Main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ID 1
+#define ID 33
 #define SOLUTION() Solution##ID()
 
 #if ID == 1
 #include "0001._Two_Sum.c"
 #define RUN_EXAMPLE Solution1();
-#endif
 
-#if ID == 1
 void Solution1(){
 	// 1. Two Sum
 	int numsSize = 4, target = 9;
 	int nums[4] = {2, 7, 11, 15};
 	int *ans = twoSum(nums, numsSize, target);
+	// int *ans = twoSum_V2(nums, numsSize, target); // using Hash Table.
 	printf(" Input: nums = [%d, %d, %d, %d], target = %d\n", 
 			nums[0], nums[1], nums[2], nums[3], target);
 	printf("Output: [%d, %d]\n", ans[0], ans[1]);
 	free(ans);
 }
 #endif
+
+#if ID == 33
+#include "0033._Search_in_Rotated_Sorted_Array.c"
+#define RUN_EXAMPLE Solution33();
+
+void Solution33(){
+	// 33. Search in Rotated Sorted Array
+	int nums[7] = {4, 5, 6, 7, 0, 1, 2};
+	printf("Output: %d\n", search(nums, 7, 0));
+	printf("Output: %d\n", search(nums, 7, 3));
+}
+#endif
+
 
 int main(int argc, char *argv[]){
 	RUN_EXAMPLE
