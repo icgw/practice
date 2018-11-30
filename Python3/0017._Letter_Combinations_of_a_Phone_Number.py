5f52a2ec9bed39e4bde8e07dc15a870e25495e46
icgw <icgw@outlook.com>
2018-11-30 15:53:16 +0800

    Letter Combinations of a Phone Number (2 ways)

--- Python3/0017._Letter_Combinations_of_a_Phone_Number.py
+++ Python3/0017._Letter_Combinations_of_a_Phone_Number.py
@@ -3,16 +3,32 @@ class Solution:
     d = {'2': "abc", '3': "def", \
             '4': "ghi", '5': "jkl", '6': "mno", \
             '7': "pqrs", '8': "tuv", '9': "wxyz"}
-
     def letterCombinations(self, digits):
         """
         :type digits: str
         :rtype: List[str]
         """
-        if not digits:
-            return []
-        if len(digits) == 1:
-            return [x for x in self.d[digits[0]]]
+        c = []
+        self.dfs(c, "", digits)
+        return c
+
+    def dfs(self, cmbs, s, r):
+        """
+        :type cmbs: List[str]
+        :type s: str
+        :type r: str
+        """
+        if not r: return cmbs.append(s) if s else None
+        for ch in self.d[r[0]]: 
+             self.dfs(cmbs, s + ch, r[1:])
+
+    def letterCombinations_pre1(self, digits):
+        """
+        :type digits: str
+        :rtype: List[str]
+        """
+        if not digits: return []
+        if len(digits) == 1: return [x for x in self.d[digits[0]]]
         return [x + y for x in self.d[digits[0]] for y in self.letterCombinations(digits[1:])]
 
 if __name__ == "__main__":
