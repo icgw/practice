183323c2197afa410d396bce2bfcb16231b35318
icgw <icgw@outlook.com>
2018-12-02 18:04:47 +0800

    update

--- Python3/0002._Add_Two_Numbers.py
+++ Python3/0002._Add_Two_Numbers.py
@@ -9,6 +9,26 @@ class Solution:
         :rtype: ListNode
         """
         dummy = ListNode(0)
+        ptr, s = dummy, 0
+        while l1 or l2 or s >= 10:
+            s //= 10
+            if l1:
+                s += l1.val
+                l1 = l1.next
+            if l2:
+                s += l2.val
+                l2 = l2.next
+            ptr.next = ListNode(s % 10)
+            ptr = ptr.next
+        return dummy.next
+
+    def addTwoNumbers_pre(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = ListNode(0)
         current, carry = dummy, 0
         while l1 or l2 or carry:
             val = carry;
