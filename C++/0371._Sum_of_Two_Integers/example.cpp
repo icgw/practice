a50fe45750b444a31f4b565ec44865a5537b0024
icgw <icgw@outlook.com>
2019-01-19 21:59:00 +0800

    Sum of Two Integers

---
+++ C++/0371._Sum_of_Two_Integers/example.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "solution.hpp"
+
+void run_example() {
+    Solution s;
+    std::cout << s.getSum(1, 2) << std::endl;
+    std::cout << s.getSum(-2, 3) << std::endl;
+}
