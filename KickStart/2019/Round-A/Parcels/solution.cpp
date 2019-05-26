26054fc6616e23deee700d019c138d3a79c8d92d
icgw <icgw@outlook.com>
2019-05-26 09:38:09 +0800

    First try: TLE

--- KickStart/2019/Round-A/Parcels/solution.cpp
+++ KickStart/2019/Round-A/Parcels/solution.cpp
@@ -8,12 +8,50 @@
 
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
-int min_overall(vector<vector<char>> &sq) {
-  int ret = 0;
-  // TODO:
+/*{{{ TIme Limit Exceeded */
+bool is_bound(int r, int c, int R, int C) {
+  return r < 0 || c < 0 || r >= R || c >= C;
+}
 
+int search_min(vector<vector<char>> &sq, int r, int c, int R, int C) {
+  static const vector<vector<int>> dirs { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+
+  vector<vector<bool>> visited(R, vector<bool> (C, false));
+  queue<vector<int>> q;
+  q.push({ r, c });
+
+  visited[r][c] = true;
+  int walk = 0;
+  while (!q.empty()) {
+    int sz = q.size();
+    for (int k = 0; k < sz; ++k) {
+      auto curr = q.front();
+      if (sq[curr[0]][curr[1]] == '1') return walk;
+      q.pop();
+      for (auto &d : dirs) {
+        int rr = curr[0] + d[0], cc = curr[1] + d[1];
+        if (!is_bound(rr, cc, R, C) && !visited[rr][cc]) {
+          visited[rr][cc] = true;
+          q.push({ rr, cc });
+        }
+      }
+    }
+    walk++;
+  }
+  return walk;
+}
+
+int min_overall(vector<vector<char>> &sq, int R, int C) {
+  int ret = 0;
+  for (int i = 0; i < R; ++i) {
+    for (int j = 0; j < C; ++j) {
+      if (sq[i][j] == '1') continue;
+      ret = max(ret, search_min(sq, i, j, R, C));
+    }
+  }
   return ret;
 }
 
@@ -29,9 +67,20 @@ int main()
         cin >> grid[j][k];
       }
     }
-    // TODO:
-    int ans = min_overall(grid);
-    cout << "Case #" << i << ": " << ans << "\n";
+
+    int ans = 501;
+    bool flag = false;
+    for (int i = 0; i < R; ++i) {
+      for (int j = 0; j < C; ++j) {
+        if (grid[i][j] == '1') continue;
+        flag = true;
+        grid[i][j] = '1';
+        ans = min(ans, min_overall(grid, R, C));
+        grid[i][j] = '0';
+      }
+    }
+    cout << "Case #" << i << ": " << (flag ? ans : 0) << "\n";
   }
+
   return 0;
-}
+}/*}}}*/
