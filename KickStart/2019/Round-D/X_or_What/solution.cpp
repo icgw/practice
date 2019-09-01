64464e66b48b44826a4971aee26e4463629acdfe
icgw <icgw@outlook.com>
2019-09-01 15:20:43 +0800

    X or What? (AC)

--- KickStart/2019/Round-D/X_or_What/solution.cpp
+++ KickStart/2019/Round-D/X_or_What/solution.cpp
@@ -8,6 +8,7 @@
 
 #include <iostream>
 #include <vector>
+#include <set>
 using namespace std;
 
 bool isEvenBits(int num) {
@@ -25,57 +26,48 @@ bool isEvenBits(int num) {
   return (cnt % 2 == 0);
 }
 
-int countEvenBits(vector<int> nums) {
-  int cnt = 0;
-  int n = nums.size();
-  for (int i = 0; i < n; ++i) {
-    if (isEvenBits(nums[i])) {
-      cnt = i + 1;
-    } else {
-      for (int k = 0; k < i - cnt; ++k) {
-        if (isEvenBits(nums[k] ^ nums[i])) {
-          cnt = i - k - 1;
-          break;
-        }
-      }
-    }
-  }
-  return cnt;
-}
-
-int main() {
+int main()
+{
   int T; cin >> T;
   for (int i = 1; i <= T; ++i) {
     int N, Q; cin >> N >> Q;
+
     vector<int> nums ( N );
-
-    for (int k = 0; k < N; ++k) cin >> nums[k];
-
-    vector<int> xors ( N, 0 );
-    int mask = 0;
+    set<int> siOddBits; int tmp;
     for (int k = 0; k < N; ++k) {
-      mask   ^= nums[k];
-      xors[k] = mask;
+      cin >> tmp; nums[k] = tmp;
+      if (!isEvenBits(tmp)) {
+        siOddBits.insert(k);
+      }
     }
 
+    int idx, val;
+    bool bPrev, bNow;
     vector<int> ans ( Q );
-    for (int k = 0; k < Q; ++k) {
-      int idx, val;
-      cin >> idx >> val;
 
-      int m = (nums[idx] ^ val);
-      nums[idx] = val;
-      for (int j = idx; j < N; ++j) {
-        xors[j] ^= m;
+    for (int k = 0; k < Q; ++k) {
+      cin >> idx >> val;
+      bPrev = isEvenBits(nums[idx]);
+      bNow  = isEvenBits(val);
+
+      if (bPrev && !bNow) {
+        siOddBits.insert(idx);
+      } else if (!bPrev && bNow) {
+        siOddBits.erase(idx);
       }
 
-      ans[k] = countEvenBits(xors);
+      int sz = siOddBits.size();
+      if (sz % 2 == 0) {
+        ans[k] = N;
+      } else {
+        ans[k] = max( *siOddBits.rbegin(), N - *siOddBits.begin() - 1 );
+      }
+      nums[idx] = val;
     }
 
-    cout << "Case #" << i << ": ";
+    cout << "Case #" << i << ":";
     for (int k = 0; k < Q; ++k) {
-      if (k != 0) cout << " ";
-      cout << ans[k];
+      cout << " " << ans[k];
     }
     cout << "\n";
   }
