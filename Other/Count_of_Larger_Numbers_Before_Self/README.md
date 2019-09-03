45e2426d469c304080ce0759c623de4b1d02408e
icgw <icgw@outlook.com>
2019-09-03 22:41:02 +0800

    under consideration

---
+++ Other/Count_of_Larger_Numbers_Before_Self/README.md
@@ -0,0 +1,12 @@
+Position: Software Engineer, Tools and Infrastructure
+
+Given an integer array `nums`. All elements are in range `[1-n]`. Return a new array `counts` where `counts[i]` is the number of larger elements to the left of `nums[i]`.
+
+# Example:
+```
+  Input: [4, 2, 1, 3, 5]
+  Output: [0, 1, 2, 1, 0]
+```
+
+# Follow-up:
+Given an array counts, return the original array.
---
+++ Other/Count_of_Larger_Numbers_Before_Self/solution.cpp
@@ -0,0 +1,24 @@
+/*
+ * solution.cpp
+ * Copyright (C) 2019 Guowei Chen <icgw@outlook.com>
+ *
+ * Distributed under terms of the GPL license.
+ */
+
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> solve(vector<int> nums) {
+  // TODO:
+  return { };
+}
+
+int main()
+{
+  vector<int> nums { 4, 2, 1, 3, 5 };
+  vector<int> counts = solve(nums); // { 0, 1, 2, 1, 0 }
+  return 0;
+}
