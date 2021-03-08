5541eff9d65acff072cfcd538f8b6e48218b18a0
Guowei Chen <icgw@outlook.com>
2021-03-08 14:41:01 +0800

    1448. Count Good Nodes in Binary Tree

--- LeetCode/Rust/src/lib.rs
+++ LeetCode/Rust/src/lib.rs
@@ -1,6 +1,39 @@
+use std::rc::Rc;
+use std::cell::RefCell;
 pub struct Solution;
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+  pub val: i32,
+  pub left: Option<Rc<RefCell<TreeNode>>>,
+  pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+  #[inline]
+  pub fn new(val: i32) -> Self {
+    TreeNode {
+      val,
+      left: None,
+      right: None
+    }
+  }
+}
+
+pub fn create_node(val: i32) -> Option<Rc<RefCell<TreeNode>>> {
+    return Some(Rc::new(RefCell::new(TreeNode::new(val))));
+}
+
+pub fn create_tree(val: i32, left: Option<Rc<RefCell<TreeNode>>>, right: Option<Rc<RefCell<TreeNode>>>) -> Option<Rc<RefCell<TreeNode>>> {
+    return Some(Rc::new(RefCell::new(TreeNode {
+        val,
+        left,
+        right,
+    })));
+}
+
 pub mod _0001_two_sum_;
 pub mod _0709_to_lower_case_;
 pub mod _0905_sort_array_by_parity_;
 pub mod _1775_equal_sum_arrays_with_minimum_number_of_operations_;
 pub mod _1647_minimum_deletions_to_make_character_frequencies_unique_;
+pub mod _1448_count_good_nodes_in_binary_tree_;
