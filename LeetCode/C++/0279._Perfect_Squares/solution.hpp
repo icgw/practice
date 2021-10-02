5ac1d7a921bfaf2e6f1d69f1433eed91ac482717
Guowei Chen <icgw@outlook.com>
2021-10-02 20:01:23 +0800

    279. Perfect Squares

--- LeetCode/C++/0279._Perfect_Squares/solution.hpp
+++ LeetCode/C++/0279._Perfect_Squares/solution.hpp
@@ -4,18 +4,18 @@ using std::min;
 using std::max;
 
 class Solution {
-    public:
-        int numSquares(int n){
-            if (n <= 0) return 0;
-            static vector<int> dp {0};
-            int m = dp.size();
-            dp.resize(max(m, n + 1), INT_MAX);
-            for (int i = 1; i * i <= n; ++i) dp[i * i] = 1;
-            for (int i = max(1, m); i <= n; ++i){
-                for (int p = 1, q = i - 1; p <= q; ++p, --q){
-                    dp[i] = min(dp[i], dp[p] + dp[q]);
-                }
-            }
-            return dp[n];
-        }
+public:
+  int numSquares(int n){
+    static vector<int> result{0};
+    int m = result.size();
+    while (m <= n) {
+      int cnt = INT_MAX;
+      for (int i = 1; i * i <= m; ++i) {
+        cnt = min(cnt, result[m - i * i] + 1);
+      }
+      result.push_back(cnt);
+      ++m;
+    }
+    return result[n];
+  }
 };
