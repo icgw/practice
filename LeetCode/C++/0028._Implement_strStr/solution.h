7a393649cd823eed0cf512a087575e3b5ecb7bc5
Guowei Chen <icgw@outlook.com>
2021-10-18 21:19:20 +0800

    update 28 with KMP algo.

--- LeetCode/C++/0028._Implement_strStr/solution.h
+++ LeetCode/C++/0028._Implement_strStr/solution.h
@@ -10,23 +10,42 @@
 
 #include <string>
 using std::string;
+#include <vector>
+using std::vector;
 
 class Solution {
+private:
+  vector<int> get_max_num_of_eq_pref_suf_str(const string& s) {
+    int m = s.size();
+    vector<int> max_eq_pref_suff(m, 0);
+    for (int i = 1, j = 0; i < m; ++i) {
+      while (j > 0 && s[i] != s[j]) {
+        j = max_eq_pref_suff[j - 1];
+      }
+      if (s[i] == s[j]) {
+        ++j;
+      }
+      max_eq_pref_suff[i] = j;
+    }
+    return max_eq_pref_suff;
+  }
 public:
   int strStr(string haystack, string needle) {
-    if (needle.empty()) {
+    int m = needle.size();
+    if (m == 0) {
       return 0;
     }
+    auto lps = get_max_num_of_eq_pref_suf_str(needle);
     int n = haystack.size();
-    int m = needle.size();
-    for (int i = 0; i < n - m + 1; ++i) {
-      for (int j = 0; j < m; ++j) {
-        if (haystack[i + j] != needle[j]) {
-          break;
-        }
-        if (j == m - 1) {
-          return i;
-        }
+    for (int i = 0, j = 0; i < n; ++i) {
+      while (j > 0 && haystack[i] != needle[j]) {
+        j = lps[j - 1];
+      }
+      if (haystack[i] == needle[j]) {
+        ++j;
+      }
+      if (j == m) {
+        return (i - m + 1);
       }
     }
     return -1;
