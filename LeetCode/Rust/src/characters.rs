59477ecbc3e7ee9fb042885a5310b2a1aab31244
Guowei Chen <icgw@outlook.com>
2024-11-15 16:35:05 +0800

    [Rust] 13. Roman to Integer

--- LeetCode/Rust/src/characters.rs
+++ LeetCode/Rust/src/characters.rs
@@ -22,6 +22,31 @@ impl CharactersAlgorithm {
         }
         return s.len() as i32 - reserved_characters;
     }
+
+    pub fn roman_to_int(s: String) -> i32 {
+        let mut ans: i32 = 0;
+        let mut last = '_';
+        for c in s.chars() {
+            match c {
+                'I' => ans += 1,
+                'V' => ans += 5,
+                'X' => ans += 10,
+                'L' => ans += 50,
+                'C' => ans += 100,
+                'D' => ans += 500,
+                'M' => ans += 1000,
+                _ => continue,
+            }
+            match last {
+                'I' => if c == 'V' || c == 'X' { ans -= 2; },
+                'X' => if c == 'L' || c == 'C' { ans -= 20; },
+                'C' => if c == 'D' || c == 'M' { ans -= 200; },
+                _ => ()
+            }
+            last = c;
+        }
+        ans
+    }
 }
 
 #[cfg(test)]
@@ -44,5 +69,16 @@ mod _leetcode1647_ {
     fn case3() {
         let s = String::from("ceabaacb");
         assert_eq!(CharactersAlgorithm::min_deletions(s), 2);
+    }
+}
+
+#[cfg(test)]
+mod _leetcode13_ {
+    use crate::characters::CharactersAlgorithm;
+
+    #[test]
+    fn case1() {
+        let s = String::from("MCMXCIV");
+        assert_eq!(CharactersAlgorithm::roman_to_int(s), 1994);
     }
 }
